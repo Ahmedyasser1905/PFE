dd81fe71f0d1b0bde92d4ff2552e7f19a37d0895
Khaled Oudjdi <koudjdi3@gmail.com>
2026-03-08 05:08:37 +0100

    Auto-sync at 2026-03-08 05:08:37

--- sync.bat
+++ sync.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Checking for changes...
-cd "C:\Users\User\Desktop\Application mobile projet\MapNote\PFE_MapNote"
+cd "C:\Users\User\.gemini\antigravity\scratch\PFE_MapNote_New"
 powershell -ExecutionPolicy Bypass -File "./sync.ps1"
 pause
